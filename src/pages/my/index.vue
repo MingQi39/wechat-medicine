31d971ffa27c68098d100b1b6c5f4b8883002a23
前端Qi <hmq039@163.com>
2025-01-31 18:15:41 +0800

    feat: 完成用户登录和退出功能

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,15 +1,27 @@
 <template>
   <view class="my-container">
-    <view class="user-info">
-      <image class="avatar" src="/static/default-avatar.svg" mode="aspectFill"></image>
+    <!-- 修改登录区域，使用button组件 -->
+    <view class="login-container" v-if="!userInfo.nickName">
+      <button class="user-info login-button" open-type="chooseAvatar" @chooseavatar="onChooseAvatar">
+        <image class="avatar" :src="avatarUrl || '/static/default-avatar.svg'" mode="aspectFill"></image>
+      </button>
       <view class="info">
-        <text class="nickname">未登录</text>
-        <text class="tips">点击登录</text>
+        <input type="nickname" class="nickname-input" placeholder="请输入昵称" @change="onNicknameChange" />
+        <text class="tips">点击头像和输入昵称</text>
+      </view>
+    </view>
+
+    <!-- 已登录状态显示 -->
+    <view v-else class="user-info">
+      <image class="avatar" :src="userInfo.avatarUrl" mode="aspectFill"></image>
+      <view class="info">
+        <text class="nickname">{{ userInfo.nickName }}</text>
+        <text class="tips">已登录</text>
       </view>
     </view>
 
     <view class="menu-list">
-      <view class="menu-item" v-for="(item, index) in menuList" :key="index">
+      <view class="menu-item" v-for="(item, index) in menuList" :key="index" @tap="handleMenuClick(item)">
         <text class="menu-text">{{ item.text }}</text>
         <text class="arrow">></text>
       </view>
@@ -18,9 +30,67 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import { onShow } from "@dcloudio/uni-app";
 
-const menuList = ref([{ text: "联系客服" }, { text: "设置" }]);
+interface UserInfo {
+  avatarUrl: string;
+  nickName: string;
+}
+
+const menuList = ref([
+  { text: "联系客服", type: "service" },
+  { text: "设置", type: "settings" },
+]);
+const userInfo = reactive<UserInfo>({
+  avatarUrl: "",
+  nickName: "",
+});
+
+const avatarUrl = ref("");
+
+// 修改检查登录状态的函数
+onShow(() => {
+  const storedUserInfo = uni.getStorageSync("userInfo");
+  if (storedUserInfo) {
+    Object.assign(userInfo, JSON.parse(storedUserInfo));
+  } else {
+    // 重置用户信息
+    userInfo.avatarUrl = "";
+    userInfo.nickName = "";
+    avatarUrl.value = "";
+  }
+});
+
+// 选择头像回调
+const onChooseAvatar = (e: any) => {
+  avatarUrl.value = e.detail.avatarUrl;
+};
+
+// 昵称输入回调
+const onNicknameChange = (e: any) => {
+  userInfo.nickName = e.detail.value;
+  if (avatarUrl.value) {
+    userInfo.avatarUrl = avatarUrl.value;
+    // 保存用户信息
+    uni.setStorageSync("userInfo", JSON.stringify(userInfo));
+    uni.showToast({
+      title: "设置成功",
+      icon: "success",
+    });
+  }
+};
+
+// 添加菜单点击处理函数
+const handleMenuClick = (item: { text: string; type: string }) => {
+  if (item.type === "settings") {
+    uni.navigateTo({
+      url: "/pages/my/settings",
+    });
+  } else if (item.type === "service") {
+    // 处理联系客服逻辑
+  }
+};
 </script>
 
 <style lang="scss">
@@ -31,30 +101,50 @@ const menuList = ref([{ text: "联系客服" }, { text: "设置" }]);
   .user-info {
     display: flex;
     align-items: center;
-    padding: 40rpx 30rpx;
+    padding: 30rpx;
     background-color: #ffffff;
+  }
 
-    .avatar {
-      width: 120rpx;
-      height: 120rpx;
-      border-radius: 50%;
-      margin-right: 30rpx;
+  .login-container {
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    background-color: #ffffff;
+  }
+
+  // 登录按钮样式重置
+  .login-button {
+    margin: 0;
+    border: none;
+    border-radius: 0;
+    padding-right: 0;
+
+    &::after {
+      border: none;
+    }
+  }
+
+  .avatar {
+    width: 120rpx;
+    height: 120rpx;
+    border-radius: 50%;
+    margin-right: 30rpx;
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+
+    .nickname {
+      font-size: 32rpx;
+      font-weight: bold;
+      margin-bottom: 10rpx;
     }
 
-    .info {
-      display: flex;
-      flex-direction: column;
-
-      .nickname {
-        font-size: 32rpx;
-        font-weight: bold;
-        margin-bottom: 10rpx;
-      }
-
-      .tips {
-        font-size: 24rpx;
-        color: #999;
-      }
+    .tips {
+      font-size: 24rpx;
+      color: #999;
     }
   }
 
@@ -84,5 +174,12 @@ const menuList = ref([{ text: "联系客服" }, { text: "设置" }]);
       }
     }
   }
+}
+
+.nickname-input {
+  font-size: 32rpx;
+  font-weight: bold;
+  margin-bottom: 10rpx;
+  width: 200rpx;
 }
 </style>
---
+++ src/pages/my/settings.vue
@@ -0,0 +1,53 @@
+<template>
+  <view class="settings-container">
+    <view class="menu-list">
+      <!-- 可以在这里添加其他设置项 -->
+      <button class="logout-btn" @tap="handleLogout">退出登录</button>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+const handleLogout = () => {
+  uni.showModal({
+    title: "提示",
+    content: "确定要退出登录吗？",
+    success: (res) => {
+      if (res.confirm) {
+        // 清除用户信息
+        uni.removeStorageSync("userInfo");
+        uni.showToast({
+          title: "已退出登录",
+          icon: "success",
+        });
+        // 返回上一页
+        setTimeout(() => {
+          uni.navigateBack();
+        }, 1500);
+      }
+    },
+  });
+};
+</script>
+
+<style lang="scss">
+.settings-container {
+  min-height: 100vh;
+  background-color: #f5f5f5;
+
+  .menu-list {
+    margin-top: 20rpx;
+    padding: 0 30rpx;
+  }
+
+  .logout-btn {
+    margin-top: 30rpx;
+    background-color: $uni-color-primary;
+    color: #fff;
+
+    &:active {
+      opacity: 0.8;
+    }
+  }
+}
+</style>
